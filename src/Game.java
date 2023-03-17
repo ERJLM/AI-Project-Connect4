acd8b3bb40b3c3d8d37d564e9b831de977eac164
ERJLM <99908664+ERJLM@users.noreply.github.com>
2023-03-17 11:37:10 +0000

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,1 +1,26 @@
+public class Game {
+    private Node node;
+
+    Game(Node n){
+        node = n;
+    }
+    
+
+
+
+//This method verifies if the game ended.
+boolean terminationTest(){
+  
+  return false;
+}
+
+/*This method verifies if you won, lost or if it was a draw.
+ (Method to use after the game has ended)
+*/
+int utility(){
+   
+    return 0;
+  }
+
+}
 
